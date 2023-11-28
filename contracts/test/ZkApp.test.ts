bce64b22e5f4cd507b4111837225190cfcc02b7a
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-28 15:08:23 +0200

    Update ZKP functionalities

--- contracts/test/ZkApp.test.ts
+++ contracts/test/ZkApp.test.ts
@@ -1,1 +1,13 @@
-
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import {
+  Verifier,
+  Verifier__factory,
+  ZkApp,
+  ZkApp__factory,
+} from "../typechain";
+import { ZKPClient, EdDSA } from "circuits";
+import { BigNumber } from "ethers";
+import fs from "fs";
+import path from "path";
